71fd61124248dddefe5256542d9db9cfb0e2c17d
Eklavya Sood <eklavyasood@gmail.com>
2024-10-16 11:12:26 +0530

    implemented fullgaps patch

--- config.h
+++ config.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 5;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -31,6 +32,7 @@ static const Rule rules[] = {
 	{ "gimp",                NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",             NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "discord",             NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "vesktop",             NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "whatsapp-for-linux",  NULL,       NULL,       1 << 5,       0,           -1 },
 	{ "Spotify",             NULL,       NULL,       1 << 8,       0,           -1 },
 };
@@ -63,7 +65,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *discordcmd[]  = { "discord", NULL };
+static const char *discordcmd[]  = { "vesktop", NULL };
 static const char *spotcmd[]  = { "spotify", NULL };
 static const char *emotecmd[]  = { "emote", NULL };
 
@@ -93,6 +95,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ MODKEY,                       XK_b,      spawn,          SHCMD("$HOME/dotfiles/.settings/browser.sh") },
 	{ MODKEY,                       XK_d,      spawn,          {.v = discordcmd } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = spotcmd } },
